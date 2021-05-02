9ac8b70ec328e6840d050b1627d11ff95db81bad
Qubo Qin <qubo.qin.2018@gmail.com>
2021-05-02 10:15:10 +0800

    add PAYMENT_TYPE

--- src/platform/index.ts
+++ src/platform/index.ts
@@ -4,3 +4,10 @@ export enum ServerEnvTag {
   TEST = 'test',
   DEV = 'development',
 }
+
+export enum PAYMENT_TYPE {
+  WECHAT = 'wechat',
+  ALIPAY = 'alipay',
+  STRIPE_ONLINE = 'stripe_online',
+  STRIPE_TEST = 'stripe_test',
+}
