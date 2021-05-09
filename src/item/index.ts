009138e737389a26bffb57cad80fe804e0301047
Qubo Qin <qubo.qin.2018@gmail.com>
2021-05-09 10:55:46 +0800

    add dynamodb-data-mapper-annotations

--- src/item/index.ts
+++ src/item/index.ts
@@ -1,19 +1,25 @@
 import { v4 as uuidv4 } from 'uuid'
 import { Entity, Column, PrimaryColumn, ManyToOne } from 'typeorm'
 
+import { attribute } from '@aws/dynamodb-data-mapper-annotations'
+
 import { User } from '../user'
 
 @Entity()
 export class Item {
   @PrimaryColumn({ default: uuidv4() })
+  @attribute({ defaultProvider: () => uuidv4() })
   itemId: string
 
   @Column({ nullable: true })
+  @attribute()
   name?: string
 
   @Column({ type: 'bigint', default: new Date().getTime() })
+  @attribute({ defaultProvider: () => new Date().getTime() })
   createdAt?: number
 
   @ManyToOne(() => User, (user) => user.items)
+  @attribute()
   user?: User
 }
--- src/user/index.ts
+++ src/user/index.ts
@@ -1,6 +1,8 @@
 import { v4 as uuidv4 } from 'uuid'
 import { Entity, Column, PrimaryColumn, OneToMany } from 'typeorm'
 
+import { attribute } from '@aws/dynamodb-data-mapper-annotations'
+
 import { Item } from '../item'
 
 export enum UserGender {
@@ -17,38 +19,49 @@ export enum UserType {
 @Entity()
 export class User {
   @PrimaryColumn({ default: uuidv4() })
+  @attribute({ defaultProvider: () => uuidv4() })
   userId: string
 
   @Column()
+  @attribute()
   phone: string
 
   @Column({ type: 'bigint', default: new Date().getTime() })
+  @attribute({ defaultProvider: () => new Date().getTime() })
   registerAt?: number
 
   @Column({ type: 'bigint', default: new Date().getTime() })
+  @attribute({ defaultProvider: () => new Date().getTime() })
   lastLoginAt?: number
 
   @Column({ nullable: true })
+  @attribute()
   firstName?: string
 
   @Column({ nullable: true })
+  @attribute()
   lastName?: string
 
   @Column({ nullable: true })
+  @attribute()
   email?: string
 
   @Column({ nullable: true })
+  @attribute()
   birthday?: string
 
   @Column({ type: 'enum', enum: UserGender, default: UserGender.UNKNOWN })
+  @attribute()
   gender?: UserGender
 
   @Column({ nullable: true })
+  @attribute()
   avatorUrl?: string
 
   @OneToMany(() => Item, (item) => item.user, {
     cascade: true,
     nullable: true,
   })
+  @attribute()
   items?: Item[]
 }
