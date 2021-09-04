578d3416b04d1a6ea6dd944ef160d12f6594e36c
Qubo Qin <qubo.qin.2018@gmail.com>
2021-09-04 22:17:50 +0800

    add customerId into order

--- src/order/index.ts
+++ src/order/index.ts
@@ -29,6 +29,9 @@ export class Order {
   @Column({ type: 'real', default: 0.0 })
   totalPrice: number
 
+  @Column()
+  customerId: string
+
   @Column({ type: 'real', default: 0.0 })
   tax: number
 
