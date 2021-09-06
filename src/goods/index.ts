9062e70f6fb595bb705f6bcf080480e66b420cf2
Qubo Qin <quboqin0710@gmail.com>
2021-09-06 11:07:41 +0800

    add category in goods

--- src/goods/index.ts
+++ src/goods/index.ts
@@ -1,9 +1,22 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm'
 
+export enum Category {
+  ALL = '全部',
+  FRUIT = '水果',
+  VEGETABLES = '蔬菜',
+  MEAT = '肉类',
+  SEA_FOOD = '海鲜',
+  SNACK_DIM_SUM = '面食点心',
+  INSTANT_NOODLES = '方便面',
+}
+
 @Entity()
 export class Good {
   @PrimaryGeneratedColumn()
   id: number
+
+  @Column({ type: 'enum', enum: Category })
+  category: Category
 
   @Column()
   name: string
