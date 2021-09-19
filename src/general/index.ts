e2022da8f2cc37c0577579d2ae745c029750f2fc
Qubo Qin <qubo.qin.2018@gmail.com>
2021-09-19 18:55:12 +0800

    message can be null

--- src/general/index.ts
+++ src/general/index.ts
@@ -1,11 +1,12 @@
 enum ERROR_CODE {
-  UNKNOW_ERROR = 'UNKNOW_ERROR',
+  NO_ERROR,
+  UNKNOW_ERROR,
 }
 
 declare interface BODY {
   code: ERROR_CODE
-  data: unknown
-  message: string
+  data?: unknown
+  message?: string
 }
 
 export { BODY, ERROR_CODE }
